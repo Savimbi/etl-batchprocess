425c3f642c6b710f91335fd72078db22b66220da
Dukuze <dudosart@Dominiques-MacBook-Pro.local>
2023-03-12 23:22:38 +0900

    Starting design phase

---
+++ src/main/java/com/dorna/eltdatawarehouse/EltDataWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.dorna.eltdatawarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EltDataWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EltDataWarehouseApplication.class, args);
+    }
+
+}
